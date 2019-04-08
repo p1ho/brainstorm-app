4bcfc5d2697036c2f2bd432d826b022cba9c3b90
hopokuan <hopokuan@umich.edu>
2019-04-08 18:17:50 -0400

    Style-fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Brainstorm Prototype' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Brainstorm Prototype' })
+})
 
-module.exports = router;
+module.exports = router
--- websocket/server.js
+++ websocket/server.js
@@ -1,13 +1,13 @@
 // test variables
 var roomSize = 2
 var currentRound = 1
-var countDownTime = 1
+var countDownTime = 3
 var usersReady = {}
 var usersData = {}
 var usersLookup = {}
 var countDown // storing setInterval()
 
-var ws = require('ws');
+var ws = require('ws')
 var events = require('events')
 var customEvents = new events.EventEmitter()
 
@@ -15,8 +15,9 @@ var customEvents = new events.EventEmitter()
 
 // Websocket server
 
-var WebSocketServer = ws.Server,
-ws = new WebSocketServer({port: 80})
+var WebSocketServer = ws.Server
+
+var ws = new WebSocketServer({ port: 80 })
 var uid = 0
 ws.on('connection', function (client) {
   uid++
@@ -28,14 +29,14 @@ ws.on('connection', function (client) {
     message = JSON.parse(raw)
     console.log('received from "%s": [%s] %s', message.name, message.type, message.message)
 
-    if (typeof(message) !== "object") {
+    if (typeof (message) !== 'object') {
       client.send(JSON.stringify({
-        type: "error",
+        type: 'error',
         message: "Message has to be of type 'object'"
       }))
     } else {
       switch (message.type) {
-        case "register":
+        case 'register':
           usersReady[message.name] = false
           usersLookup[this.id] = message.name
           customEvents.emit('newUser', {
@@ -44,15 +45,15 @@ ws.on('connection', function (client) {
           })
 
           client.send(JSON.stringify({
-            type: "success",
-            message: "You are connected and registered!",
+            type: 'success',
+            message: 'You are connected and registered!',
             users: usersReady
           }))
 
           console.log(usersReady)
-        break
+          break
 
-        case "ready":
+        case 'ready':
           usersReady[message.name] = true
           customEvents.emit('readyStateChange', {
             message: 'user changed ready state',
@@ -85,9 +86,9 @@ ws.on('connection', function (client) {
             }
           }
 
-        break
+          break
 
-        case "unready":
+        case 'unready':
           usersReady[message.name] = false
           customEvents.emit('readyStateChange', {
             message: 'user changed ready state',
@@ -99,47 +100,46 @@ ws.on('connection', function (client) {
             clearInterval(countDown)
             customEvents.emit('countDownStop')
           }
-        break
+          break
 
-        case "submission":
+        case 'submission':
           switch (message.roundNum) {
             case 1:
               usersData[message.name] = {
                 roundOneIdeas: message.data
               }
               client.send(JSON.stringify({
-                type: "success",
-                message: "Your form was successfully submitted and recorded",
+                type: 'success',
+                message: 'Your form was successfully submitted and recorded',
                 users: usersReady
               }))
-            break
+              break
             case 2:
               usersData[message.name].roundTwoIdeas = message.data
               client.send(JSON.stringify({
-                type: "success",
-                message: "Your form was successfully submitted and recorded",
+                type: 'success',
+                message: 'Your form was successfully submitted and recorded',
                 users: usersReady
               }))
-            break
+              break
             case 3:
               usersData[message.name].votedFor = message.data
               // handle vote result from user
               client.send(JSON.stringify({
-                type: "over",
-                message: "The session is over, terminating and sending final results",
+                type: 'over',
+                message: 'The session is over, terminating and sending final results',
                 data: ['some-final-results']
               }))
-            break
+              break
           }
           console.log(message.data)
           console.log('-------------------------------------------------------')
-        break
-
+          break
       }
     }
   })
 
-  client.on('close', function(e) {
+  client.on('close', function (e) {
     customEvents.emit('connectionClose', {
       message: 'user disconnected',
       user: usersLookup[this.id],
@@ -155,8 +155,8 @@ ws.on('connection', function (client) {
 customEvents.on('newUser', function (ev) {
   ws.clients.forEach(client => {
     client.send(JSON.stringify({
-      type: "newUser",
-      message: "New user connected to room",
+      type: 'newUser',
+      message: 'New user connected to room',
       user: ev.name,
       users: usersReady
     }))
@@ -166,8 +166,8 @@ customEvents.on('newUser', function (ev) {
 customEvents.on('readyStateChange', function (ev) {
   ws.clients.forEach(client => {
     client.send(JSON.stringify({
-      type: "readyStateChange",
-      message: "Someone changed ready state",
+      type: 'readyStateChange',
+      message: 'Someone changed ready state',
       user: ev.name,
       users: usersReady
     }))
@@ -178,8 +178,8 @@ customEvents.on('countDown', function (ev) {
   if (ev.timeLeft > 0) {
     ws.clients.forEach(client => {
       client.send(JSON.stringify({
-        type: "countDown",
-        message: "Counting down before round starts",
+        type: 'countDown',
+        message: 'Counting down before round starts',
         timeLeft: ev.timeLeft
       }))
     })
@@ -190,18 +190,18 @@ customEvents.on('countDown', function (ev) {
     switch (currentRound) {
       case 1:
         formData = []
-      break
+        break
       case 2:
         formData = usersData
-      break
+        break
       case 3:
         formData = usersData
-      break
+        break
     }
     ws.clients.forEach(client => {
       client.send(JSON.stringify({
-        type: "roundStart",
-        message: "Starting round",
+        type: 'roundStart',
+        message: 'Starting round',
         roundNum: currentRound,
         data: formData
       }))
@@ -213,8 +213,8 @@ customEvents.on('countDown', function (ev) {
 customEvents.on('countDownStop', function (ev) {
   ws.clients.forEach(client => {
     client.send(JSON.stringify({
-      type: "countDownStop",
-      message: "Stop the countdown"
+      type: 'countDownStop',
+      message: 'Stop the countdown'
     }))
   })
 })
@@ -225,8 +225,8 @@ customEvents.on('connectionClose', function (ev) {
   delete usersLookup[ev.userId]
   ws.clients.forEach(client => {
     client.send(JSON.stringify({
-      type: "userDisconnect",
-      message: "Someone disconnected",
+      type: 'userDisconnect',
+      message: 'Someone disconnected',
       users: usersReady
     }))
   })
@@ -234,17 +234,17 @@ customEvents.on('connectionClose', function (ev) {
 
 // =============================================================================
 
-//operations for detecting dropped connections
+// operations for detecting dropped connections
 
-function noop() {}
+function noop () {}
 
-function heartbeat() {
+function heartbeat () {
   this.isAlive = true
 }
 
 var hasDropped = false
-const interval = setInterval(function ping() {
-  ws.clients.forEach(function each(client) {
+const interval = setInterval(function ping () {
+  ws.clients.forEach(function each (client) {
     if (client.isAlive === false) {
       hasDropped = true
       delete usersReady[ev.user]
@@ -255,11 +255,11 @@ const interval = setInterval(function ping() {
     client.ping(noop)
   })
   if (hasDropped) {
-    console.log("Someone dropped, notifying all other clients")
-    ws.clients.forEach(function each(client) {
+    console.log('Someone dropped, notifying all other clients')
+    ws.clients.forEach(function each (client) {
       client.send(JSON.stringify({
-        type: "userDisconnect",
-        message: "Someone disconnected",
+        type: 'userDisconnect',
+        message: 'Someone disconnected',
         users: usersReady
       }))
     })
