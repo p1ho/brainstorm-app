2086859b4576b72ab6af97a3b6ee204500c69d81
hopokuan <hopokuan@umich.edu>
2019-04-09 11:36:24 -0400

    Refactored to look cleaner

--- websocket/handlers/eventEmitter.js
+++ websocket/handlers/eventEmitter.js
@@ -3,14 +3,21 @@
 var events = require('events')
 var eventEmitter = new events.EventEmitter()
 
-eventEmitter.on('pong', require('./actions/pong'))
-eventEmitter.on('error', require('./actions/error'))
-eventEmitter.on('newUser', require('./actions/newUser'))
-eventEmitter.on('readyStateChange', require('./actions/readyStateChange'))
-eventEmitter.on('submission', require('./actions/submission'))
-eventEmitter.on('countDown', require('./actions/countDown'))
-eventEmitter.on('countDownStop', require('./actions/countDownStop'))
-eventEmitter.on('userDisconnect', require('./actions/userDisconnect'))
-eventEmitter.on('disconnectUser', require('./actions/disconnectUser'))
+var eventList = [
+  'pong',
+  'error',
+  'newUser',
+  'readyStateChange',
+  'submission',
+  'countDown',
+  'countDownStop',
+  'collectionComplete',
+  'userDisconnect',
+  'disconnectUser'
+]
+
+for (let eventname of eventList) {
+  eventEmitter.on(eventname, require(`./actions/${eventname}`))
+}
 
 module.exports = eventEmitter
