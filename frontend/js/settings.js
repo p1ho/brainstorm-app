0850f967ad0b4c46a5c211885afc829043c784db
hopokuan <hopokuan@umich.edu>
2019-04-09 23:56:02 -0400

    Updated settings

--- frontend/js/settings.js
+++ frontend/js/settings.js
@@ -1,8 +1,8 @@
 'use strict'
 
-module.exports.seeSelf = true
-module.exports.minRoundOne = .1
-module.exports.minRoundTwo = .1
-module.exports.minRoundVote = .1
+module.exports.seeSelf = false
+module.exports.minRoundOne = 6
+module.exports.minRoundTwo = 6
+module.exports.minRoundVote = 3
 module.exports.numOfIdeas = 3
 module.exports.maxVote = 3
